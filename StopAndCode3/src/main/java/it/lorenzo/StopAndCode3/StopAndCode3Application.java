36e6f18f935bf3ea917984e4bbaa018dae7a2e98
Lorenzo Mason <mason.lorenzo18@gmail.com>
2025-07-11 18:17:19 +0200

    Creazione progetto Spring Boot iniziale con dipendenze Web, JPA e H2

---
+++ StopAndCode3/src/main/java/it/lorenzo/StopAndCode3/StopAndCode3Application.java
@@ -0,0 +1,13 @@
+package it.lorenzo.StopAndCode3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopAndCode3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopAndCode3Application.class, args);
+	}
+
+}
---
+++ StopAndCode3/src/test/java/it/lorenzo/StopAndCode3/StopAndCode3ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lorenzo.StopAndCode3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopAndCode3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
